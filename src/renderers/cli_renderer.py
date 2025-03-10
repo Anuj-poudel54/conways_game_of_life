ae9ec5c74c6f667f92524e6c0fe90feac25767cd
Anuj poudel <anujpoudel54@gmail.com>
2025-03-10 14:46:53 +0545

    implemented cli renderer renderer

--- src/renderers/cli_renderer.py
+++ src/renderers/cli_renderer.py
@@ -1,4 +1,14 @@
 from .base_renderer import Renderer
 
+import time, os
+
 class CLIRenderer(Renderer):
-    ...
+    def __init__(self, grid = None, surface_size = ...):
+        super().__init__(grid, surface_size)
+        self._wrap_around = True
+
+    def render(self):
+        os.system("cls")
+        self._dprint_grid()
+        self._calculate_next_gen()
+        time.sleep(.2)
