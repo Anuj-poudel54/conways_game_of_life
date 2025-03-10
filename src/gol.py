1e1e3724dc20d1427476d62a046b4532440e6a0d
Anuj poudel <anujpoudel54@gmail.com>
2025-03-10 14:46:17 +0545

    added flag for getting renderer type and file path

--- src/gol.py
+++ src/gol.py
@@ -9,14 +9,27 @@ from pathlib import Path
 cwd = Path(os.getcwd())
 # getting file name
 argv = sys.argv[1:]
+
+renderer_selecter = 'gui'
 file_path = None
+
 if argv:
-    file_path = (cwd / argv[0] )
-    file_path = file_path if os.path.isfile(file_path) else None
+    i = 0
+    while i < len(argv):
+        if argv[i].lower() == '-r':
+            renderer_selecter = argv[i+1]
+
+        if argv[i].lower() == '-f':
+            file_path = (cwd / argv[i+1] )
+            file_path = file_path if os.path.isfile(file_path) else None
+
+        i += 2
+
 
 if __name__ == "__main__":
-    renderer_class = renderer_factory("gui")
-    renderer = renderer_class(surface_size=(1000, 1000))
+    renderer_class = renderer_factory(renderer_selecter)
+    renderer = renderer_class(surface_size=(500, 500))
+    print(file_path)
     if file_path:
         renderer.grid = load_game_rle(file_path,  renderer.grid)
     renderer.start_loop()
