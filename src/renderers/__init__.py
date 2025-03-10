4f430caa46ca85571b63a11aed281609fd82310e
Anuj poudel <anujpoudel54@gmail.com>
2025-03-10 15:40:09 +0545

    import GUIRenderer conditionally to prevent terminal greeting in CLI mode

--- src/renderers/__init__.py
+++ src/renderers/__init__.py
@@ -1,7 +1,6 @@
 from typing import Literal
 
 from .base_renderer import Renderer
-from .gui_renderer import GUIRenderer
 from .cli_renderer import CLIRenderer
 
 def renderer_factory(renderer_type: Literal["cli"] | Literal["gui"]) -> type[Renderer]:
@@ -9,4 +8,6 @@ def renderer_factory(renderer_type: Literal["cli"] | Literal["gui"]) -> type[Ren
     if renderer_type == "cli": 
         return CLIRenderer
     else:  
+        #importing so that on selecting cli mode, greeting from pygame won't show on terminal.
+        from .gui_renderer import GUIRenderer 
         return GUIRenderer
