e94d13aecde0a7119dd2bd7897582f662c09acf2
Anuj poudel <anujpoudel54@gmail.com>
2025-03-10 15:40:00 +0545

    refactor renderer selection variable and improve CLI error handling

--- src/gol.py
+++ src/gol.py
@@ -10,14 +10,14 @@ cwd = Path(os.getcwd())
 # getting file name
 argv = sys.argv[1:]
 
-renderer_selecter = 'gui'
+renderer_mode = 'gui'
 file_path = None
 
 if argv:
     i = 0
     while i < len(argv):
         if argv[i].lower() == '-r':
-            renderer_selecter = argv[i+1]
+            renderer_mode = argv[i+1]
 
         if argv[i].lower() == '-f':
             file_path = (cwd / argv[i+1] )
@@ -27,8 +27,17 @@ if argv:
 
 
 if __name__ == "__main__":
-    renderer_class = renderer_factory(renderer_selecter)
-    renderer = renderer_class(surface_size=(500, 500))
+    if renderer_mode == 'cli' and not file_path:
+        print("ERROR: Need to provide pattern file path if using 'cli' mode. ")
+        print("       Use '-f <file_path>' flag for passing file.")
+        exit(-1)
+
+    surface_size=(1000, 1000)
+    if renderer_mode == 'cli':
+        surface_size=(500, 500)
+    
+    renderer_class = renderer_factory(renderer_mode)
+    renderer = renderer_class(surface_size=surface_size)
     if file_path:
         renderer.grid = load_game_rle(file_path,  renderer.grid)
     renderer.start_loop()
